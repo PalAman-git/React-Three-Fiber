546ecbaa14248aee051268bbe66bf41d4d5afc58
Aman <Palaman2626@gmail.com>
2023-01-26 03:12:28 +0530

    Mouse Events

---
+++ Mouse Events/src/Experience.jsx
@@ -0,0 +1,46 @@
+import { useFrame } from "@react-three/fiber";
+import { meshBounds, OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+
+export default function Experience() {
+  const cube = useRef();
+
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+  });
+
+  const eventHandler = (event) => {
+    cube.current.material.color.set(`hsl(${Math.random() * 360},100%,75%)`);
+  };
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh position-x={-2} onClick={(event) => event.stopPropagation()}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh 
+      ref={cube} 
+      raycast={meshBounds} //tests in sphere around the geometry
+      position-x={2} 
+      scale={1.5} 
+      onClick={eventHandler} 
+      onPointerEnter={()=>{document.body.style.cursor='pointer'}} onPointerLeave={()=>{document.body.style.cursor='default'}}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color={"red"} />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+}
